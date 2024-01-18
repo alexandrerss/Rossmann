9bb2fe0bb40578da020b6927a64e9b4b6d5eb9ba
alexandrerss <alexandrerss@gmail.com>
2024-01-18 09:58:09 -0300

    mudanca para porta 10 mil

--- api/rossmann-api.py
+++ api/rossmann-api.py
@@ -122,5 +122,5 @@ def index():
         return '<h1> Telegram BOT de previsão de vendas das lojas Rossmann</h1>'
 
 if __name__ == '__main__':
-    port = os.environ.get( 'PORT', 5000 )
+    port = os.environ.get( 'PORT', 10000 )
     app.run( host='0.0.0.0', port=port )
